e3051c78dd58305d13a732048adb66bb202dfe51
Jeffrey Drake <jeffrey@madebyjeffrey.com>
2020-07-12 18:41:22 -0400

    Trying out rollup

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,26 @@
 var path = require('path');
 
-module.exports = {    
+module.exports = {
     entry: {
-        parser: './src/index',        
+        parser: './src/index',
     },
-    mode: "development",
-    target: "node",
+    mode: 'development',
+    target: 'node',
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js'
+        path: path.resolve(__dirname, 'lib'),
+        filename: '[name].js',
     },
     resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.json']
+        extensions: ['.ts', '.tsx', '.js', '.json'],
     },
     module: {
-        rules: [{
-            // Include ts, tsx, js, and jsx files.
-            test: /\.(ts|js)x?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-        }],
-    }
+        rules: [
+            {
+                // Include ts, tsx, js, and jsx files.
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+        ],
+    },
 };
